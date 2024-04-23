c6e134f8805aaa5a0b6159f4d30c99188248109b
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-04-23 12:18:31 -0600

    Create close_match and refactor match add answer
    Created close_match logic, and refactored match to prevent false
    positives. Also added a method for generating an answer.

--- mastermind.rb
+++ mastermind.rb
@@ -3,13 +3,9 @@
 # Contains the game methods for use in the game. Some will have increased privacy to prevent the
 # user from accessing the winning code
 class Game
-  scoreboard = Hash.new([' ', ' ', ' ', ' '])
-  results = Hash.new([0, 0])
-  round = 1
-  win = false
-  guess = []
 
-  def display_board(board, results) # show the board so far.
+  # show the board so far.
+  def display_board(board, results)
     i = 1
     12.times do
       puts board[i], ' | ', results[i]
@@ -17,7 +13,8 @@ class Game
     end
   end
 
-  def decoder_submit(round) # get an array of decoder guesses
+  # get an array of decoder guesses
+  def decoder_submit(round)
     g = []
     i = 1
     puts "Round #{round}.\nChoose from the following: R = red, O = orange, Y = Yellow, G = green, B = blue, P = purple"
@@ -30,7 +27,8 @@ class Game
     g
   end
 
-  def entry_checker(string) # make sure each entry is valid.
+  # make sure each entry is valid.
+  def entry_checker(string)
     colors = %w[R O Y G B P]
     unless colors.any?(string)
       puts 'Invalid choice.'
@@ -40,11 +38,60 @@ class Game
     string
   end
 
-  def match?(guess, answer) # check each item in the guess array to see whether each index exactly matches the answer
+  # check each item in the guess array to see whether each index exactly matches the answer
+  def match?(guess, answer)
     m = 0
     guess.each_with_index do |v, i|
-      if guess[i] == answer[i] {m += 1}
+      next unless v == answer[i]
+
+      m += 1
+      answer.delete_at(i)
+      guess.delete_at(i)
     end
-    m
+    c = close_match?(guess, answer)
+    [m, c]
+  end
+
+  # find and return values that are present in answer, but in a different position.
+  def close_match?(guess, answer)
+    c = 0
+    guess.each_with_index do |v, i|
+      next unless answer.any?(v)
+
+      answer.delete_at(answer.index(v))
+      guess.delete_at(i)
+      c += 1
+    end
+    c
+  end
+
+  def generate_answer
+    a = []
+    colors = %w[R O Y G B P]
+    4.times do
+      a << colors[rand(6)]
+    end
+    a
+  end
+end
+
+scoreboard = Hash.new([' ', ' ', ' ', ' '])
+results = Hash.new([0, 0])
+round = 1
+win = false
+
+# begin game
+answer = Game.generate_answer
+while win == false || round <= 12
+  Game.display_board(scoreboard, results)
+  guess = Game.decoder_submit(round)
+  scoreboard[round] = guess
+  results[round] = Game.match?(guess, answer)
+  if guess == answer
+    win = true
+    puts 'You win!'
+    Game.display_board(scoreboard, results)
+  else
+    round += 1
   end
 end
